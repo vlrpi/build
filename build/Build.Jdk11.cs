b1eb19d0533df877c29548f6ed33df865c418cea
vova-lantsov-dev <vova.lantsov.dev@gmail.com>
2021-06-30 15:53:32 +0300

    Add support for arm/v6 and arm/v7 architectures

--- build/Build.Jdk11.cs
+++ build/Build.Jdk11.cs
@@ -15,7 +15,7 @@ partial class Build
             foreach (var (tags, dockerfile) in tagsToBuild)
             {
                 DockerBuildxBuild(_ => _
-                    .SetPlatform("linux/arm64")
+                    .SetPlatform("linux/arm64,linux/arm/v7,linux/arm/v6")
                     .SetTag(tags)
                     .EnableRm()
                     .SetPath(dockerfile.Parent)
